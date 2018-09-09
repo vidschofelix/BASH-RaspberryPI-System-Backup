c6307801c9c1b75ed8ca05e6eafeba27c08b3668
Starscream <felix.com@gmx.com>
2018-09-09 16:15:28 +0200

    replace deletion by age with number of files

--- system_backup.sh
+++ system_backup.sh
@@ -2,20 +2,20 @@
 #
 # Automate Raspberry Pi Backups
 #
-# Vidschofelix
+# Vidschofelix 2018
 # Forked from Kristofer Källsbo 2017 www.hackviking.com
 #
-# Usage: system_backup.sh {path} {days of retention}
+# Usage: system_backup.sh {path} {files_to_keep}
 #
 # Below you can set the default values if no command line args are sent.
 # The script will name the backup files {$HOSTNAME}.{YYYYmmddHHmm}.img.gz
-# When the script deletes backups older then the specified retention
+# When the script deletes backups before the specifier number of files to keep
 # it will only delete files with it's own $HOSTNAME.
 #
 
 # Declare vars and set standard values
 backup_path=/mnt/backup
-retention_days=3
+files_to_keep=3
 
 # Check that we are root!
 if [[ ! $(whoami) =~ "root" ]]; then
@@ -33,7 +33,7 @@ if [ ! -z $1 ]; then
 fi
 
 if [ ! -z $2 ]; then
-   retention_days=$2
+   files_to_keep=$2
 fi
 
 # Create trigger to force file system consistency check if image is restored
@@ -46,4 +46,4 @@ dd bs=1M if=/dev/mmcblk0 | pigz -c --fast -p 3 > ${backup_path}/$HOSTNAME.$(date
 rm /boot/forcefsck
 
 # Delete old backups
-find ${backup_path}/$HOSTNAME.*.img.gz -mtime +${retention_days} -type f -delete
+ls -t ${backup_path}/$HOSTNAME.*.img.gz | tail -n +`expr ${files_to_keep}+1` | xargs rm --
